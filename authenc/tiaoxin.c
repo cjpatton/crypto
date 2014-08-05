40e31baa65118092846bb79022460808234ed44e
Christopher Patton <chrispatton@gmail.com>
2014-08-05 13:08:33 -0700

    Encrypt/decrypt last block.
    TODO arbitrary length messages and authenticaiton.

--- authenc/tiaoxin.c
+++ authenc/tiaoxin.c
@@ -8,6 +8,7 @@
 #include <tmmintrin.h>
 #include <string.h>
 #include <stdio.h>
+#include <assert.h>
 
 #define ALIGN(n) __attribute__ ((aligned(n)))
 
@@ -92,7 +93,7 @@ void update(TiaoxinState *state, __m128i M0, __m128i M1, __m128i M2)
   
   T = state->T4; 
   
-  tmp[0] = _mm_aesenc_si128(T[3], T[0]) ^ M0; 
+  tmp[0] = _mm_aesenc_si128(T[3], T[0]) ^ M1; 
   tmp[1] = _mm_aesenc_si128(T[0], *(__m128i*)Z0); 
   tmp[2] = T[1]; 
   tmp[3] = T[2]; 
@@ -104,7 +105,7 @@ void update(TiaoxinState *state, __m128i M0, __m128i M1, __m128i M2)
   
   T = state->T6; 
   
-  tmp[0] = _mm_aesenc_si128(T[5], T[0]) ^ M0; 
+  tmp[0] = _mm_aesenc_si128(T[5], T[0]) ^ M2; 
   tmp[1] = _mm_aesenc_si128(T[0], *(__m128i*)Z0); 
   tmp[2] = T[1]; 
   tmp[3] = T[2]; 
@@ -135,6 +136,7 @@ void init(TiaoxinState *state, const Byte K[], const Byte N[])
   
   for (int i = 0; i < 16; i++)
     buff[i] = N[i]; 
+
   state->T3[2] = _mm_loadu_si128((__m128i*)buff); 
   state->T4[2] = _mm_loadu_si128((__m128i*)buff); 
   state->T6[2] = _mm_loadu_si128((__m128i*)buff); 
@@ -157,9 +159,39 @@ void encrypt(Byte *C,
              const Byte N[])
 {
   TiaoxinState state; 
-  init(&state, K, N); 
-  disp(&state);
+  ALIGN(16) Byte buff [16]; 
+  __m128i M0, M1, C0, C1;  
+  unsigned i; 
+
+  assert(msg_len <= 32 /* Do last chunk only for now. */);
+  //disp(&state); 
   
+  init(&state, K, N); 
+  
+  *(__m128i*)buff = _mm_setzero_si128(); 
+  for (i = 0; i < 16 && i < msg_len; i++)
+    buff[i] = M[i]; 
+  M0 = _mm_loadu_si128((__m128i*)buff);   
+
+  *(__m128i*)buff = _mm_setzero_si128(); 
+  for (; i < msg_len; i++)
+    buff[i - 16] = M[i]; 
+  M1 = _mm_loadu_si128((__m128i*)buff);   
+ 
+  update(&state, M0, M1, M0 ^ M1); 
+  C0 = state.T3[0] ^ state.T3[2] ^ state.T4[1] ^ (state.T6[3] & state.T4[3]);
+  C1 = state.T6[0] ^ state.T4[2] ^ state.T3[1] ^ (state.T6[5] & state.T3[2]);
+    
+  _mm_storeu_si128((__m128i*)buff, C0);  
+  for (i = 0; i < 16 && i < msg_len; i++)
+    C[i] = buff[i];
+  
+  _mm_storeu_si128((__m128i*)buff, C1);  
+  for (; i < msg_len; i++)
+    C[i] = buff[i - 16];
+  
+  /* TODO authenticate */ 
+
 }
 
 int decrypt(Byte *M, 
@@ -170,6 +202,42 @@ int decrypt(Byte *M,
             const Byte K[], 
             const Byte N[])
 {
+  TiaoxinState state; 
+  ALIGN(16) Byte buff [16]; 
+  __m128i M0, M1, C0, C1, zero = _mm_setzero_si128(); 
+  unsigned i; 
+
+  assert(msg_len <= 32 /* Do last chunk only for now. */);
+  //disp(&state); 
+  
+  init(&state, K, N); 
+
+  *(__m128i*)buff = _mm_setzero_si128(); 
+  for (i = 0; i < 16 && i < msg_len; i++)
+    buff[i] = C[i]; 
+  C0 = _mm_loadu_si128((__m128i*)buff);   
+
+  *(__m128i*)buff = _mm_setzero_si128(); 
+  for (; i < msg_len; i++)
+    buff[i - 16] = C[i]; 
+  C1 = _mm_loadu_si128((__m128i*)buff);   
+  
+  update(&state, zero, zero, zero); 
+  M0 = C0 ^ state.T3[0] ^ state.T3[2] ^ state.T4[1] ^ (state.T6[3] & state.T4[3]); 
+  M1 = C1 ^ state.T6[0] ^ state.T4[2] ^ state.T3[1] ^ (state.T6[5] & state.T3[2]) ^ M0;
+  state.T3[0] ^= M0; 
+  state.T4[0] ^= M1; 
+  state.T6[0] ^= M0 ^ M1;
+
+  _mm_storeu_si128((__m128i*)buff, M0);  
+  for (i = 0; i < 16 && i < msg_len; i++)
+    M[i] = buff[i];
+  
+  _mm_storeu_si128((__m128i*)buff, M1);  
+  for (; i < msg_len; i++)
+    M[i] = buff[i - 16];
+
+  /* TODO authenticate */ 
   return 0; 
 }
 
@@ -185,12 +253,30 @@ int main(int argc, const char **argv)
 {
 
   Byte key [] =   {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};  
-  Byte nonce [] = {0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0}; 
+  Byte nonce [] = {0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 1}; 
 
-  Byte message [] = "Hi there, my name is Chris.";
-  Byte ciphertext [1024], tag [16]; 
+  Byte message [] = "Hello, my name is Chris!!";
+  Byte ciphertext [1024], plaintext [1024], tag [16]; 
+  unsigned i, msg_len = strlen((const char *)message); 
 
-  encrypt(ciphertext, tag, message, strlen((const char *)message), 16, key, nonce); 
+  encrypt(ciphertext, tag, message, msg_len, 16, key, nonce); 
+  decrypt(plaintext, ciphertext, tag, msg_len, 16, key, nonce); 
+  
+  printf("Message:    "); 
+  for (i = 0; i < msg_len; i++)
+    printf("%02x", message[i]); 
+  printf(" (%d bytes)\n", msg_len); 
+
+  printf("Plaintext:  "); 
+  for (i = 0; i < msg_len; i++)
+    printf("%02x", plaintext[i]); 
+  printf("\n"); 
+  
+  printf("Ciphertext: "); 
+  for (i = 0; i < msg_len; i++)
+    printf("%02x", ciphertext[i]); 
+  printf("\n"); 
+  
 
 
 
