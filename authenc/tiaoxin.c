3693079222c6fda1964cad05ec8deda8d2894786
Christopher Patton <chrispatton@gmail.com>
2014-08-09 17:49:47 -0700

    Tiaoxin authentication.

--- authenc/tiaoxin.c
+++ authenc/tiaoxin.c
@@ -1,7 +1,10 @@
 /* 
  * tiaoxin.c -- Tiaoxin-346, a Caesar submission. 
- * TODO Is it OK to pass an unaligned byte buffer to AES load instruction? 
- * TODO Authentication. 
+ *
+ * NOTE Is it OK to pass an unaligned byte buffer to AES load instruction? 
+ *      Yes! _mm_load_si128() is the aligned version; I'm using 
+ *      _mm_loadu_si128().
+ *
  * TODO Processing of additional data. 
  */
 
@@ -77,6 +80,13 @@ void disp(const TiaoxinState *state)
   printf("+------------------------------------------+\n"); 
 }
 
+void print128_num(__m128i var)
+{
+      uint16_t *val = (uint16_t*) &var;
+          printf("Numerical: %i %i %i %i %i %i %i %i \n", 
+                         val[0], val[1], val[2], val[3], val[4], val[5], 
+                                    val[6], val[7]);
+}
 
 void update(TiaoxinState *state, __m128i M0, __m128i M1, __m128i M2)
 {
@@ -125,23 +135,16 @@ void update(TiaoxinState *state, __m128i M0, __m128i M1, __m128i M2)
 
 void init(TiaoxinState *state, const Byte K[], const Byte N[]) 
 {
-  ALIGN(16) Byte buff [16]; 
-  for (int i = 0; i < 16; i++)
-    buff[i] = K[i]; 
-
-  state->T3[0] = _mm_loadu_si128((__m128i*)buff); 
-  state->T3[1] = _mm_loadu_si128((__m128i*)buff); 
-  state->T4[0] = _mm_loadu_si128((__m128i*)buff); 
-  state->T4[1] = _mm_loadu_si128((__m128i*)buff); 
-  state->T6[0] = _mm_loadu_si128((__m128i*)buff); 
-  state->T6[1] = _mm_loadu_si128((__m128i*)buff); 
+  state->T3[0] = _mm_loadu_si128((__m128i*)K); 
+  state->T3[1] = _mm_loadu_si128((__m128i*)K); 
+  state->T4[0] = _mm_loadu_si128((__m128i*)K); 
+  state->T4[1] = _mm_loadu_si128((__m128i*)K); 
+  state->T6[0] = _mm_loadu_si128((__m128i*)K); 
+  state->T6[1] = _mm_loadu_si128((__m128i*)K); 
   
-  for (int i = 0; i < 16; i++)
-    buff[i] = N[i]; 
-
-  state->T3[2] = _mm_loadu_si128((__m128i*)buff); 
-  state->T4[2] = _mm_loadu_si128((__m128i*)buff); 
-  state->T6[2] = _mm_loadu_si128((__m128i*)buff); 
+  state->T3[2] = _mm_loadu_si128((__m128i*)N); 
+  state->T4[2] = _mm_loadu_si128((__m128i*)N); 
+  state->T6[2] = _mm_loadu_si128((__m128i*)N); 
   
   state->T4[3] = _mm_loadu_si128((__m128i*)Z0); 
   state->T6[3] = _mm_loadu_si128((__m128i*)Z1); 
@@ -150,6 +153,28 @@ void init(TiaoxinState *state, const Byte K[], const Byte N[])
 
   for (int i = 0; i < 16; i++)
     update(state, *(__m128i*)Z0, *(__m128i*)Z1, *(__m128i*)Z0); 
+}
+
+void auth(Byte *T, unsigned tag_len, unsigned msg_len, TiaoxinState *state)
+{
+  ALIGN(16) Byte buff [16]; 
+  __m128i AD, M;
+  unsigned i;
+  AD = _mm_setzero_si128(); ((unsigned *)&AD)[0] = 0/* ad_len */; 
+  M = _mm_setzero_si128();  ((unsigned *)&M)[0] = msg_len;
+  
+  update(state, AD, M, AD ^ M); 
+  for (i = 0; i < 20; i++)
+    update(state, *(__m128i*)Z1, *(__m128i*)Z0, *(__m128i*)Z1); 
+
+  M = state->T3[0] ^ state->T3[1] ^ state->T3[2] ^ 
+      state->T4[0] ^ state->T4[1] ^ state->T4[2] ^ state->T4[3] ^
+      state->T6[0] ^ state->T6[1] ^ state->T6[2] ^ state->T6[3] ^ 
+        state->T6[4] ^ state->T6[5]; 
+
+  _mm_storeu_si128((__m128i*)buff, M);  
+  for (i = 0; i < tag_len; i++)
+    T[i] = buff[i]; 
 }
 
 void encrypt(Byte *C, 
@@ -204,9 +229,14 @@ void encrypt(Byte *C,
   for (; i < msg_len; i++)
     C[i] = buff[i - j - 16];
   
-  /* TODO authenticate */ 
-
+  /* Generate tag */ 
+  auth(T, tag_len, msg_len, &state); 
+  printf("Tag:   "); 
+  for (i = 0; i < tag_len; i++)
+    printf("%02x", T[i]); 
+  printf("\n"); 
 }
+
 
 int decrypt(Byte *M, 
             const Byte *C, 
@@ -240,6 +270,7 @@ int decrypt(Byte *M,
     _mm_storeu_si128((__m128i*)&M[j + 16], M1);  
   }
 
+  /* Fragmented last block */  
   *(__m128i*)buff = _mm_setzero_si128(); 
   for (i = j; i < 16 + j && i < msg_len; i++)
     buff[i - j] = C[i]; 
@@ -253,20 +284,35 @@ int decrypt(Byte *M,
   update(&state, zero, zero, zero); 
   M0 = C0 ^ state.T3[0] ^ state.T3[2] ^ state.T4[1] ^ (state.T6[3] & state.T4[3]); 
   M1 = C1 ^ state.T6[0] ^ state.T4[2] ^ state.T3[1] ^ (state.T6[5] & state.T3[2]) ^ M0;
-  state.T3[0] ^= M0; 
-  state.T4[0] ^= M1; 
-  state.T6[0] ^= M0 ^ M1;
 
-  _mm_storeu_si128((__m128i*)buff, M0);  
+  _mm_storeu_si128((__m128i*)buff, M0); 
   for (i = j; i < 16 + j && i < msg_len; i++)
+  {
+    ((Byte *)&(state.T3[0]))[i - j] ^= buff[i - j];  
+    ((Byte *)&(state.T6[0]))[i - j] ^= buff[i - j];  
     M[i] = buff[i - j];
-  
-  _mm_storeu_si128((__m128i*)buff, M1);  
-  for (; i < msg_len; i++)
-    M[i] = buff[i - j - 16];
+  }
 
-  /* TODO authenticate */ 
-  return 0; 
+  _mm_storeu_si128((__m128i*)buff, M1);  
+  for (; i < msg_len; i++) 
+  {
+    ((Byte *)&(state.T4[0]))[i - j - 16] ^= buff[i - j - 16];  
+    ((Byte *)&(state.T6[0]))[i - j - 16] ^= buff[i - j - 16];  
+    M[i] = buff[i - j - 16];
+  }
+
+  /* Verify tag. If rejected, destroy plaintext. */ 
+  auth(buff, tag_len, msg_len, &state); 
+  printf("  Verify: "); 
+  for (i = 0; i < tag_len; i++)
+    printf("%02x", buff[i]); 
+  printf("\n"); 
+  if (strncmp((const char *)buff, (const char *)T, tag_len) != 0)
+  {
+    //memset(M, 0, msg_len * sizeof(Byte)); 
+    return 0; 
+  }
+  else return 1; 
 }
 
 
@@ -283,12 +329,12 @@ int main(int argc, const char **argv)
   Byte key [] =   {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};  
   Byte nonce [] = {0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 1}; 
 
-  Byte message [] = "0000000000000000000000000000000000000000000000000000000000000";
+  Byte message [] = "This is an actual message, I'm serious. This is not a test. ";
   Byte ciphertext [1024], plaintext [1024], tag [16]; 
-  unsigned i, msg_len = strlen((const char *)message); 
+  unsigned i, msg_len = strlen((const char *)message), tag_len=4; 
 
-  encrypt(ciphertext, tag, message, msg_len, 16, key, nonce); 
-  decrypt(plaintext, ciphertext, tag, msg_len, 16, key, nonce); 
+  encrypt(ciphertext, tag, message, msg_len, tag_len, key, nonce); 
+  unsigned valid = decrypt(plaintext, ciphertext, tag, msg_len, tag_len, key, nonce); 
   
   printf("Message:    "); 
   for (i = 0; i < msg_len; i++)
@@ -298,6 +344,7 @@ int main(int argc, const char **argv)
   printf("Plaintext:  "); 
   for (i = 0; i < msg_len; i++)
     printf("%02x", plaintext[i]); 
+  if (!valid) printf(" rejected!");
   printf("\n"); 
   
   printf("Ciphertext: "); 
