751ec63c894ae5970f2fc6407c5ba75e8579fc41
Christopher Patton <chrispatton@gmail.com>
2014-08-24 14:34:08 -0700

    Working .

--- authenc/fella.c
+++ authenc/fella.c
@@ -12,6 +12,8 @@ typedef union {
   Word word [4]; 
 } Block; 
 
+typedef Byte OBlock [16]; 
+
 #define reverse_u32(n) ( \
  ((n & 0x000000ffu) << 24) | \
  ((n & 0x0000ff00u) <<  8) | \
@@ -26,20 +28,56 @@ typedef union {
   X.word[3] = reverse_u32(X.word[3]); \
 }
 
+#define Oreverse_u32(dst, src) { \
+    *u32_ptr(dst) = ((*u32_ptr(src) & 0x000000ffu) << 24) | \
+                    ((*u32_ptr(src) & 0x0000ff00u) <<  8) | \
+                    ((*u32_ptr(src) & 0x00ff0000u) >>  8) | \
+                    ((*u32_ptr(src) & 0xff000000u) >> 24); \
+}
+
+#define u8_ptr(X) ((uint8_t *)X)
+#define u32_ptr(X) ((uint32_t *)X) 
+   
+#define Oset_big_endian(X) { \
+ Oreverse_u32(&u8_ptr(X)[0],  &u8_ptr(X)[0]);  \
+ Oreverse_u32(&u8_ptr(X)[4],  &u8_ptr(X)[4]);  \
+ Oreverse_u32(&u8_ptr(X)[8],  &u8_ptr(X)[8]);  \
+ Oreverse_u32(&u8_ptr(X)[12], &u8_ptr(X)[12]); \
+      }
+
+
 static void display_block(const Block X) 
 {
-  for (int i = 0; i < 4; i ++)
+  int i; 
+  for (i = 0; i < 4; i ++)
     printf("0x%08x ", X.word[i]); 
+}
+
+static void Odisplay_block(const Byte X []) 
+{
+  int i; 
+  for (i = 0; i < 4; i ++)
+    printf("0x%08x ", *(uint32_t *)&X[i * 4]); 
 }
 
 int main() 
 {
+  int i;
   Block X;
-  for (int i = 0; i < 16; i++)
+  for (i = 0; i < 16; i++)
     X.byte[i] = i; 
-  display_block(X); printf("\n"); 
+  
+  Odisplay_block(X.byte); printf("\n"); 
   set_big_endian(X); 
-  display_block(X); printf("\n"); 
+  Odisplay_block(X.byte); printf("\n"); 
+  
+  OBlock Y;
+  for (i = 0; i < 16; i++)
+    Y[i] = i; 
+  
+  Odisplay_block(Y); printf("\n"); 
+  Oset_big_endian(Y); 
+  Odisplay_block(Y); printf("\n"); 
 
   return 0; 
 }
