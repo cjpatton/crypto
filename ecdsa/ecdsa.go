77b37621c532922a5b50361c9ee3bf928db0b70f
Christopher Patton <chrispatton@gmail.com>
2018-02-17 15:01:44 -0500

    Working on ECDSA signing.

--- ecdsa/ecdsa.go
+++ ecdsa/ecdsa.go
@@ -2,35 +2,87 @@ package ecdsa
 
 import (
 	"crypto/elliptic"
-	//"crypto/sha"
 	"crypto/rand"
+	"errors"
+	"fmt"
+	h "hash"
 	"math/big"
 )
 
 type PublicKey struct {
-	params *elliptic.CurveParams
-	x, y   *big.Int
+	curve elliptic.Curve
+	hash  h.Hash
+	x, y  *big.Int
 }
 
 type SecretKey struct {
 	PublicKey
-	k []byte
+	priv *big.Int
 }
 
-func NewKeyPair(curve elliptic.Curve) (*PublicKey, *SecretKey, error) {
+func NewKeyPair(curve elliptic.Curve, hash h.Hash) (*PublicKey, *SecretKey, error) {
+	if hash.Size() < (curve.Params().BitSize >> 3) {
+		return nil, nil, errors.New("hash output smaller than group field elements")
+	}
 	k, x, y, err := elliptic.GenerateKey(curve, rand.Reader)
 	if err != nil {
 		return nil, nil, err
 	}
-	pk := &PublicKey{curve.Params(), x, y}
-	sk := &SecretKey{*pk, k}
+	pk := &PublicKey{curve, hash, x, y}
+	sk := &SecretKey{*pk, new(big.Int)}
+	sk.priv.SetBytes(k)
 	return pk, sk, nil
 }
 
-func (sk *SecretKey) Sign(in []byte) ([]byte, error) {
-	return nil, nil
+func (sk *SecretKey) Sign(msg []byte) ([]byte, error) {
+
+	var r, s, z, x1, zero *big.Int
+
+	// Compute z, the first BitSize bits of hash(msg).
+	z = new(big.Int)
+	sk.hash.Reset()
+	sk.hash.Write(msg)
+	fmt.Println(z)
+	z.SetBytes(sk.hash.Sum(nil)[:sk.curve.Params().BitSize>>3])
+
+	zero = new(big.Int)
+	zero.SetInt64(0)
+	x1 = new(big.Int)
+	r = new(big.Int)
+	s = new(big.Int)
+
+	for {
+		// Choose a random k in [1,N).
+		//
+		// It's possible that k==0, but this occurs with negligible probability.
+		k, err := rand.Int(rand.Reader, sk.curve.Params().N)
+		if err != nil {
+			return nil, err
+		}
+
+		// Compute r.
+		x1, _ = sk.curve.ScalarBaseMult(k.Bytes())
+		r.Mod(x1, sk.curve.Params().N)
+		if r.Cmp(zero) == 0 {
+			continue
+		}
+
+		// Compute s.
+		k.ModInverse(k, sk.curve.Params().N)
+		s.Mul(r, sk.priv)
+		s.Add(s, z)
+		s.Mul(s, k)
+		s.Mod(s, sk.curve.Params().N)
+		if s.Cmp(zero) == 0 {
+			continue
+		}
+
+		break
+	}
+
+	return elliptic.Marshal(sk.curve, r, s), nil
 }
 
-func (pk *PublicKey) Verify(in, sig []byte) bool {
+func (pk *PublicKey) Verify(msg, sig []byte) bool {
 	return false
 }
--- ecdsa/ecdsa_test.go
+++ ecdsa/ecdsa_test.go
@@ -2,15 +2,32 @@ package ecdsa
 
 import (
 	"crypto/elliptic"
+	"crypto/sha256"
+	"crypto/sha512"
 	"testing"
 )
 
+func TestNewKeyPair(t *testing.T) {
+	if _, _, err := NewKeyPair(elliptic.P256(), sha256.New()); err != nil {
+		t.Error("NewKeyPair(P256, SHA512) fails:", err)
+	}
+
+	if _, _, err := NewKeyPair(elliptic.P521(), sha256.New()); err == nil {
+		t.Error("NewKeyPair(P521, SHA256) passes, expected failure")
+	}
+}
+
 func TestSignVerify(t *testing.T) {
-	pk, sk, err := NewKeyPair(elliptic.P256())
+	pk, sk, err := NewKeyPair(elliptic.P256(), sha512.New())
 	if err != nil {
 		t.Fatal("NewKeyPair() fails:", err)
 	}
-
 	_ = pk
-	_ = sk
+
+	sig, err := sk.Sign([]byte("What a wonderful world."))
+	if err != nil {
+		t.Fatal("Sign() fails:", err)
+	}
+
+	t.Log(sig)
 }
