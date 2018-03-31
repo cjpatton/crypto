cac1fdd67abc64e3453ef5218882fdf0c0fefe2f
Christopher Patton <chrispatton@gmail.com>
2018-03-31 18:23:34 -0400

    Find squares in the field.

--- silly/fun.py
+++ silly/fun.py
@@ -1,11 +1,18 @@
 #p = (1<<64) - 189
 p = (1<<32) - 5
-a = 2
-d = 3
+a = 1
+d = 19
 
 # Finding square roots in the field.
+sq = []
+nsq = []
 assert (p % 4) == 3
-b = 2343
-x = b ** ((p + 1)>>2)
-x %= p # = sqrt(b)
+for b in range(1,100):
+    x = pow(b, ((p + 1)/4), p)
+    if b == ((x*x) % p):
+        sq.append(b)
+    else:
+        nsq.append(b)
 
+print sq
+print nsq
