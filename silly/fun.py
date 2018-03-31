23e4a6fc4877c6d89a27380ca66ef1993e63eae3
Christopher Patton <chrispatton@gmail.com>
2018-03-31 16:50:38 -0400

    Finding points is gonna be hard ...

--- silly/fun.py
+++ silly/fun.py
@@ -1,6 +1,11 @@
 #p = (1<<64) - 189
 p = (1<<32) - 5
+a = 2
+d = 3
 
-for x in range(2,10000000):
-    if ((p-1) % x) == 0:
-        print x
+# Finding square roots in the field.
+assert (p % 4) == 3
+b = 2343
+x = b ** ((p + 1)>>2)
+x %= p # = sqrt(b)
+
