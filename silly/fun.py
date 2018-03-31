f6966baa00cf22f6de271a434c9d04a26c7c7176
Christopher Patton <chrispatton@gmail.com>
2018-03-31 19:36:41 -0400

    Found a better prime.

--- silly/fun.py
+++ silly/fun.py
@@ -1,7 +1,13 @@
 #p = (1<<64) - 189
-p = (1<<32) - 5
+#p = (1<<32) - 5
+p = (1<<33) - 9
 a = 1
 d = 19
+
+# Cofactors?
+for x in range(1,100000):
+    if ((p-1) % x) == 0:
+        print x
 
 # Finding square roots in the field.
 sq = []
