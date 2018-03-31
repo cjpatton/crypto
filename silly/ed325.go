2afafdc6eadb29b8e1174512c212de604bb7e1a7
Christopher Patton <chrispatton@gmail.com>
2018-03-31 15:52:41 -0400

    Compute the multiplicative inverse in the base field.

--- silly/ed325.go
+++ silly/ed325.go
@@ -1,10 +1,39 @@
 package ed325
 
 const (
-	p = (1 << 32) - 5
-	a = p - 1 // -1
-	d = 3
+	pBaseField = (1 << 32) - 5
+	aBaseField = 2 // a square in GF(p)
+	dBaseField = 3 // a non-square in GF(p)
 )
+
+func baseFieldInv0(x0, x, q uint64) uint64 {
+	var y uint64
+	y = (q * x) % pBaseField
+	if x0 > y {
+		y = x0 - y
+	} else {
+		y = x0 + pBaseField - y
+	}
+	return y % pBaseField
+}
+
+// Stolen from wikipedia
+func baseFieldInv(a uint64) uint64 {
+	var s, t, r, s0, t0, r0 uint64
+	s = 0
+	t = 1
+	r = pBaseField
+	s0 = 1
+	t0 = 0
+	r0 = a
+	for r != 0 {
+		q := r0 / r
+		r0, r = r, baseFieldInv0(r0, r, q)
+		s0, s = s, baseFieldInv0(s0, s, q)
+		t0, t = t, baseFieldInv0(t0, t, q)
+	}
+	return s0
+}
 
 type Point struct {
 	x, y uint64
@@ -28,17 +57,19 @@ func New() *Group {
 // TODO Get rid of this after picking a point encoding.
 // The only exported interface should be encoded points.
 func (g *Group) NewPoint(x, y uint64) *Point {
-	return &Point{x % p, y % p, g}
+	return &Point{x % pBaseField, y % pBaseField, g}
 }
 
 func (P *Point) isOnCurve() bool {
 	xx := P.x * P.x
-	xx %= p
+	xx %= pBaseField
 	yy := P.y * P.y
-	yy %= p
-	l := a*xx + yy
-	r := 1 + d*xx*yy
-	return (l % p) == (r % p)
+	yy %= pBaseField
+	l := aBaseField*xx + yy
+	r := xx * yy
+	r %= pBaseField
+	r = 1 + dBaseField*r
+	return (l % pBaseField) == (r % pBaseField)
 }
 
 func (P *Point) IsValid() bool {
--- silly/ed325_test.go
+++ silly/ed325_test.go
@@ -1,9 +1,28 @@
 package ed325
 
-import "testing"
+import (
+	"math/big"
+	"testing"
+)
 
 func TestIsOnCurve(t *testing.T) {
 	g := New()
 	t.Log(g.NewPoint(132, 43).IsValid())
 	t.Log(g.Id.IsValid())
+}
+
+func TestModularInverse(t *testing.T) {
+	b := uint64(43)
+	y := baseFieldInv(b)
+
+	a := new(big.Int)
+	p := new(big.Int)
+	x := new(big.Int)
+	a.SetUint64(b)
+	p.SetUint64(pBaseField)
+	x.ModInverse(a, p)
+
+	if y != x.Uint64() {
+		t.Errorf("baseFieldInv(23): got %d, expected %d", y, x.Uint64())
+	}
 }
